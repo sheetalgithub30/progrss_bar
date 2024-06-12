12a8e2564372c39b269a2a3a36dd1d4799f605ab
sheetal <sheetalkumarilife@gmail.com>
2024-06-12 14:20:51 +0530

    added styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,49 @@ import { useEffect, useState } from "react";
 import "./index.css";
 
 function App() {
-  let [count, setCount] = useState(0);
+    let [count, setCount] = useState(0);
+  const [color,setColor] = useState("");
   const [name, setName] = useState("Loading....");
 
   useEffect(() => {
+       
     const interval = setInterval(() => {
       setCount((prevCount) => {
         const newCount = prevCount + 1;
         if (newCount === 100) {
           clearInterval(interval);
-          setName("Complete!");
+          setName("Completed!!!");
         }
+
+        if(newCount < 30){
+          setColor("#ff0000");
+        }else if(newCount<80){
+           setColor("#ffff00")
+          } 
+          else{
+            setColor("#00ff00")
+          }
         return newCount;
       });
     }, 50);
 
     return () => {
-      clearInterval(interval); // Cleanup the interval on component unmount
+      clearInterval(interval); 
     };
   }, []);
+
+
 
   return (
     <>
       <h1>Progress Bar</h1>
       <div id="bar-outer">
-      <div className="bar" style={{width:`${count}%` }}>
+      <div className="bar" style={{width:`${count}% `, backgroundColor:`${color}`}}>
         <p>{count}%</p>
       </div>
       
       </div>
-      <p>{name}</p>
+      <p id="name">{name}</p>
     </>
   );
 }
